0de8c92daad50c49887bbd96ffb3adfde52946cc
Gagan Singh <gagan261115@gmail.com>
2023-04-26 22:34:54 +0530

    final commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='App_child_div'>
+        <header className='Header'>
+          <h2 >Remine India Private Ltd. </h2>
+          <ul>
+            <li>HOME</li>
+            <li>OBJECTIVES</li>
+            <li>CONTACT</li>
+          </ul>
+        </header>
+        <main>
+
+          <div className='firstdivmain '>
+            <img src="https://t4.ftcdn.net/jpg/05/55/32/99/360_F_555329972_7CNEDOvl34YykCyeoRe8dMbF5yNTcDRE.jpg"></img>
+            <h2 style={{
+              textAlign: "center"
+            }}>Building <br />Sustainable Managment</h2>
+            <img src="https://t4.ftcdn.net/jpg/05/75/28/67/360_F_575286707_XWzAVa98BubEYQjAy2LsfRWXDCgc2gJT.jpg"></img>
+          </div>
+          <div className='seconddivmain '>
+            <h2>Recycling E-Waste</h2>
+            <div className='seconddivmain_childs'>
+              <div className='seconddivmain_childs_common'>What is E-Waste?
+                <p style={{
+                  marginTop: "20px",
+                  fontSize: "18px",
+                  textAlign: "justify"
+                }}>
+                  Electronic waste (e-waste), is a generic term used to describe all types of old, end-of-life or discarded electrical and electronic equipment, such as household appliances; office information and communications equipment; entertainment and consumer electronic equipment</p>
+              </div>
+              <div className='seconddivmain_childs_common'>Why Recycle E-Waste?
+                <p style={{
+                  marginTop: "20px",
+                  fontSize: "18px",
+                  textAlign: "justify"
+                }}>
+                  The recycling of e-waste serves a lot of useful purposes. For instance, include protecting human and environmental health by keeping those devices out of landfills. Or recovering the parts within the devices that still have value, and providing manufacturers with recycled metals that can be used to make new products.</p></div>
+              <div className='seconddivmain_childs_common'>Why us?
+                <p style={{
+                  marginTop: "20px",
+                  fontSize: "18px",
+                  textAlign: "justify"
+                }}>
+                   Remine is Empowering clean and Green Nation by recycling E-waste and Li Batteries.  </p></div>
+
+            </div>
+          </div>
+
+        </main>
+        <footer>All copyrights&copy; reserved</footer>
+      </div>
     </div>
   );
 }
